c93b0027d227eb7777717871ee106e21dc333cce
SeekerRook <xrist.vardakis@gmail.com>
2022-11-27 12:27:01 +0200

    secure

--- login-page.js
+++ login-page.js
@@ -1,3 +1,5 @@
+
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
@@ -7,8 +9,13 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user1333sb" && password === "aWVlZS1udHVhLXNi") {
-        alert("You have successfully logged in.");
+    	
+	const us =  CryptoJS.MD5(username).toString() ;
+	const pd =  CryptoJS.MD5(password).toString() ;
+	if (us  === "cef62998fb5161d450e49e1b5cbf69e9" && pd === "53923320f37b92bdcb42a57fc02f35fa") {
+        console.log(us)
+	console.log(pd)
+	alert("You have successfully logged in.");
         location.href = "page.html";
     } else {
         loginErrorMsg.style.opacity = 1;
